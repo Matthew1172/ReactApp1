000d965a35463d15f9343391810a4316138c6f63
matthew <mpecko@sunyrockland.edu>
2019-09-03 18:48:52 -0700

    First commit

--- ios/ReactApp1Tests/ReactApp1Tests.m
+++ ios/ReactApp1Tests/ReactApp1Tests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface ReactApp1Tests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation ReactApp1Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
